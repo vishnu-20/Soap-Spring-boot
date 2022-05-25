f32f39a2ea11a74ace3c7aab30a8afd36112991f
unknown <vishnu-20>
2022-05-25 20:30:04 +0530

    first commit

---
+++ src/main/java/com/address/soap/api/AddressApiSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.address.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressApiSoapWsApplication{
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(AddressApiSoapWsApplication.class, args);
+	}
+}
